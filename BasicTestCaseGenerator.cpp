6d29bb7b7e719ae159917cbadd4bae50b22aec84
Erick Manuel Troyo Del Campo Díaz <troyodk@gmail.com>
2018-10-30 10:50:10 -0600

    Updated case generator

--- BasicTestCaseGenerator.cpp
+++ BasicTestCaseGenerator.cpp
@@ -1,48 +1,67 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-///Apartir de aqui normalmente inician mis codigos , si es que me estas espiando <3
-///From here normally my code starts, thats if you are stalking me <3
-string k;
-string k1;
-string vac;
+
 string caseName;
-int main ()
+string nombreDeArchivo;
+string buildCaseName(int tempNum,string typeOfCase) ///función para generar el nombre del archivo, por ejemplo "easy1.in".
 {
-    ios_base::sync_with_stdio(0);
-    long long   int  x=0,z,mal=INF,s=0,a1=0,b1=0,c1,x1,y1,t=-INF,res=0,e,f;
-    long long   int  a,b,c,d,y=1,ya=1;
-    cin>>caseName;
-    ///caseName es hard,easy,etc
-    int numeroDeCasos=10;
-    random_device rd;
-    mt19937 gen(rd()); ///Parámetro que usaremos para los random
-    uniform_int_distribution<> Nrand(1,50000),Krand(1,7000000); ///Limites de nuestros casos
+    string temporalnumber,temporalName;
+    while(tempNum)
+    {
+        temporalnumber.push_back((tempNum%10)+'0');
+        tempNum/=10;
+    }
+    reverse(temporalnumber.begin(),temporalnumber.end());
+    temporalName=caseName+temporalnumber+typeOfCase;
+    return temporalName;
+
+}
+int numeros[500000]; ///Arreglo que usaremos para el problema de ejemplo.
+int main ()
+        {
+            ios_base::sync_with_stdio(0);
+            caseName="easy"; ///caseName es hard,easy,etc
+            int numeroDeCasos=10; ///Cuantos casos haremos.
+            random_device rd;
+            mt19937 gen(rd()); ///Parámetro que usaremos para generar los random
+    uniform_int_distribution<> Nrand(1,50000),Krand(1,7000000); ///Limites de nuestros random
+    fstream buffer;
+
+    ///Ejemplo de como crear un problema para ordenar un arreglo de números aleatorios de tamaño N
     for(int casoActual=0; casoActual<numeroDeCasos; casoActual++)
     {
-        existe=vacio;
-        x=ya;
-        ya++;
-        k1=vac;
-        while(x)
+        ///Aqui crearemos un archivo .in, y ahi escribiremos la entrada
+        nombreDeArchivo=buildCaseName(casoActual+1,".in");
+        buffer.open (nombreDeArchivo.c_str(),fstream::out);
+        int n=Nrand(gen); ///Esto asigna a n un número aleatorio entre el rango 1-50000
+        buffer<<n<<endl; ///imprimiremos N seguido de un salto de linea
+        for(int i=0; i<n; i++)
         {
-            k1.push_back((x%10)+'0');
-            x/=10;
-        }
-        reverse(k1.begin(),k1.end());
-        k=caseName;
-        k+=k1;
-        k1=k;
-        k1.push_back('.');
-        k1.push_back('i');
-        k1.push_back('n');
-       // k1.push_back('o');
-       // k1.push_back('u');
-      //  k1.push_back('t');
-        fstream troy;
-        troy.open (k1.c_str(),fstream::out);
+            if(i!=0)
+                buffer<<" "; ///espacio para separar los números
 
-        troy<<
+            numeros[i]=Krand(gen);
+            buffer<<numeros[i];
+        }
+        buffer<<endl; ///Importante siempre terminar nuestros archivos con un salto de linea
+        buffer.close();
+        ///Ahora haremos la salida
+        nombreDeArchivo=buildCaseName(casoActual+1,".out");
+        buffer.open (nombreDeArchivo.c_str(),fstream::out);
+        sort(numeros,numeros+n);
+        for(int i=0; i<n; i++)
+        {
+            if(i!=0)
+                buffer<<" ";
+            buffer<<numeros[i];
+        }
+        buffer<<endl; ///También los .out terminarlos con salto de linea
+        buffer.close();
+
+
+
+
 
     }
 }
