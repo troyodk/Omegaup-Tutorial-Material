6058949bf3d878fcb908439db7e9e4c686252457
Erick Manuel Troyo Del Campo Díaz <troyodk@gmail.com>
2018-10-30 21:08:38 -0600

    Turbo mejorado por lhchavez

--- BasicTestCaseGenerator.cpp
+++ BasicTestCaseGenerator.cpp
@@ -1,61 +1,71 @@
-#include <iostream>
-#include <cstring>
-#include <algorithm>
-#include <fstream>
-using namespace std;
+    #include <iostream>
+    #include <cstring>
+    #include <algorithm>
+    #include <fstream>
+    #include <random>
 
-string case_name;
-string nombre_de_archivo;
-string buildcase_name(int temp_num,const string &type_of_case) {
-  string temporal_number, temporal_name;
-  while (temp_num) {
-    temporal_number.push_back((temp_num % 10) + '0');
-    temp_num /= 10;
-  }
-  reverse(temporal_number.begin(), temporal_number.end());
-  temporal_name = case_name + temporal_number + type_of_case;
-  return temporal_name;
-}
-int numeros[500010];  /// Arreglo que usaremos para el problema de ejemplo.
-int main() {
-  ios_base::sync_with_stdio(0);
-  case_name = "easy";  /// case_name es hard,easy,etc
-  int numero_de_casos = 10;  /// Cuantos casos haremos.
-  random_device rd;
-  mt19937 gen(rd());  /// Parámetro que usaremos para generar los random
-  uniform_int_distribution<> Nrand(1, 50000),
-      Krand(1, 7000000);  /// Limites de nuestros random
-  fstream buffer;
+    using namespace std;
 
-  /// Ejemplo de como crear un problema para ordenar un arreglo de números
-  /// aleatorios de tamaño N
-  for (int caso_actual = 0; caso_actual < numero_de_casos; caso_actual++) {
-    /// Aqui crearemos un archivo .in, y ahi escribiremos la entrada
-    nombre_de_archivo = buildcase_name(caso_actual + 1, ".in");
-    buffer.open(nombre_de_archivo.c_str(), fstream::out);
-    int n = Nrand(
-        gen);  /// Esto asigna a n un número aleatorio entre el rango 1-50000
-    buffer << n << endl;  /// imprimiremos N seguido de un salto de linea
-    for (int i = 0; i < n; i++) {
-      if (i != 0)
-        buffer << " ";  /// espacio para separar los números
+    // Parámetros de configuración.
+    constexpr const char kCaseName[] = "easy";
+    constexpr int kNumeroDeCasos = 10;
+    constexpr int kMaxN = 50000;
+    constexpr int kMaxK = 7000000;
 
-      numeros[i] = Krand(gen);
-      buffer << numeros[i];
+    string build_case_name(const string& case_name, int case_number,
+                           const string& extension) {
+      char buf[100];
+      snprintf(buf, sizeof(buf), "%s_%03d.%s", case_name.c_str(), case_number,
+               extension.c_str());
+      return string(buf);
     }
-    buffer << endl;  /// Importante siempre terminar nuestros archivos con un
-                     /// salto de linea
-    buffer.close();
-    /// Ahora haremos la salida
-    nombre_de_archivo = buildcase_name(caso_actual + 1, ".out");
-    buffer.open(nombre_de_archivo.c_str(), fstream::out);
-    sort(numeros, numeros + n);
-    for (int i = 0; i < n; i++) {
-      if (i != 0)
-        buffer << " ";
-      buffer << numeros[i];
+
+    int main() {
+      ios_base::sync_with_stdio(0);
+
+      // Fuente de números aleatorios.
+      random_device rd;
+      mt19937 gen(rd());
+
+      // Distribuciones uniformes en los intervalos [1, kMaxN] y [1, kMaxK].
+      uniform_int_distribution<> Nrand(1, kMaxN), Krand(1, kMaxK);
+
+      // Ejemplo de como crear un problema para ordenar un arreglo de números
+      // aleatorios de tamaño N.
+      for (int caso_actual = 1; caso_actual <= kNumeroDeCasos; caso_actual++) {
+      	// Creamos la entrada en memoria.
+        int numeros[kMaxN];
+      	int N = Nrand(gen);  // Esto asigna a N un número aleatorio en [1, kMaxN].
+      	for (int i = 0; i < N; i++)
+          numeros[i] = Krand(gen); // Esto asigna un número aleatorio en [1, kMaxK].
+
+
+        // Aqui crearemos un archivo .in, y ahi escribiremos la entrada.
+        {
+          ofstream f(build_case_name(kCaseName, caso_actual, "in"));
+
+          // Imprimiremos N seguido de un salto de línea.
+          f << N << "\n";
+          for (int i = 0; i < N; i++)
+            f << numeros[i] << " ";
+          // Es deseble siempre terminar los .in con un salto de línea.
+          f << "\n";
+        }
+
+        // Ahora hay que resolver el problema. En este caso, había que ordenar los
+        // enteros.
+        sort(numeros, numeros + N);
+
+        // Una vez resuelto el problema hay que escribir la salida en el .out.
+        {
+          ofstream f(build_case_name(kCaseName, caso_actual, "out"));
+
+          for (int i = 0; i < N; i++)
+            f << numeros[i] << " ";
+          // También los .out terminarlos con salto de linea
+          f << "\n";
+        }
+      }
+
+      return 0;
     }
-    buffer << endl;  /// También los .out terminarlos con salto de linea
-    buffer.close();
-  }
-}
